3dd80f085cd325c50da09bfe30e3e2034cb8ce60
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-07 21:41:43 +0000

    const orno const

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -6,7 +6,7 @@ var myVar = 333;
 // const myVar = 333;
 
 // Export the modified value (optional)
-module.exports.myVar = myVar;
+// module.exports.myVar = myVar;
 
 // Import the modified value of myVar from modifyMyVar.js
 //myVar = require('./0-javascript_is_amazing');
