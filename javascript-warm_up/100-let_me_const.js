a0bdcc6d16978f25c64d5916119314169b30b163
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-07 21:28:25 +0000

    const orno const

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,2 +1,9 @@
-//update value oy myVar
-let myVar = 333;
+// Modify the value of myVar to 333
+var myVar = 333;
+
+// You can also use 'let' or 'const' depending on your project requirements:
+// let myVar = 333;
+// const myVar = 333;
+
+// Export the modified value (optional)
+module.exports.myVar = myVar;
