abff0d48c41c406418e91a564e43f3f61048988f
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-07 21:48:03 +0000

    const orno const

---
+++ javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,16 @@
+// Define a function called "executeXTimes" that takes two parameters:
+// x (the number of times to execute the function)
+// theFunction (the function to be executed)
+function executeXTimes(x, theFunction) {
+    for (let i = 0; i < x; i++) {
+        theFunction();
+    }
+}
+
+// Example usage:
+function sayHello() {
+    console.log("Hello, World!");
+}
+
+// Call executeXTimes to execute sayHello 3 times
+executeXTimes(3, sayHello);
