c4d8da706dfded8f15da904d3f21f3a227b15066
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-07 21:13:30 +0000

    add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,4 +1,8 @@
-//print out thw integers added
-function add(a, r) {
-    return (a + r);
+// Define a function called "add" that takes two integers as parameters and returns their sum.
+function add(x, y) {
+    return x + y;
 }
+
+// Now you can call the "add" function from anywhere in your code.
+const result = add(5, 7); // This will return 12
+console.log(result); // This will print 12 to the console
