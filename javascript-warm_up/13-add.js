77ed9329ad8f85267498d9e793fa46aed309e113
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-07 21:15:23 +0000

    add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -4,5 +4,5 @@ function add(x, y) {
 }
 
 // Now you can call the "add" function from anywhere in your code.
-const result = add(5, 7); // This will return 12
-console.log(result); // This will print 12 to the console
+const result = add(4, 85);
+console.log(result); // This should print 89 to the console
