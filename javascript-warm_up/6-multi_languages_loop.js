47c55bfe0375ac8c2590572f6fcd63e7d85f9ca3
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-07 20:51:00 +0000

    loop langauge

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 let joe = ["C is fun", "Python is cool", "JavaScript is amazing"];
 let count = 0; 
-while (count <= joe.length) {
+while (count < joe.length) {
     console.log(joe[count]);
     count = count + 1;
 }
