4cd29e394725ad8d63f127dae51f9a42659e35f9
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-08 23:29:33 +0000

    rectangle#4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)) {
+            // Create an empty object if w or h is not a positive integer or is 0
+            return {};
+        }
+
+        this.width = w;
+        this.height = h;
+    }
+
+    print() {
+        if (this.width && this.height) {
+            for (let i = 0; i < this.height; i++) {
+                console.log('X'.repeat(this.width));
+            }
+        }
+    }
+
+    rotate() {
+        const temp = this.width;
+        this.width = this.height;
+        this.height = temp;
+    }
+
+    double() {
+        this.width *= 2;
+        this.height *= 2;
+    }
+}
+
+module.exports = Rectangle;
