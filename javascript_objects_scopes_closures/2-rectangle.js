3267696c28f67747927028eb81883e04435322c8
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-08 23:14:03 +0000

    rectangle#1

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+//class of rectangle identifing h and w
+class Rectangle{
+    constructor(w, h) {
+
+        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number' || !Number.isInteger(w) || !Number.isInteger(h)) {
+      // Create an empty object if w or h is not a positive integer or is 0
+      return {};
+        }
+        
+        this.width = w;
+        this.height = h;
+    }
+
+}
