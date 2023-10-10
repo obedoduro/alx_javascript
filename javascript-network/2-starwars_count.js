798957a43dcd5b25d590105abae001bd2ecd354d
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-10 12:57:14 +0000

    starwars

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -22,7 +22,7 @@ request(apiUrl, (error, response, body) => {
             movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
         );
 
-        console.log(`${wedgeAntillesMovies}`);
+        console.log(`${wedgeAntillesMovies.length}`);
     } else {
         console.error(`Error: Unable to fetch movie data. Status Code: ${response.statusCode}`);
         process.exit(1);
