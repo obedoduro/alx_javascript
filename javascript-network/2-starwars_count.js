da5dac579db4b681c7cfddfd7c7d9703faaf5b04
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-10 11:02:08 +0000

    star wars wedge antillies

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const apiUrl = process.argv[2];
+
+
+if (!apiUrl) {
+    console.log('Usage: node countWedgeAntillesMovies.js <api_url>');
+    process.exit(1);
+}
+
+const characterId = 18;
+
+request(apiUrl, (error, reponse, body) => {
+    if (error) {
+        console.log('Error:', error);
+        process.exit(1);
+    }
+
+    if (response.statusCode === 200) {
+        const moviessData = JSON.parse(body).results;
+
+
+        //Filter the movies where "Wedge Antilles" (charater ID 18) is present
+        const wedgeAntillesMovies = moviesData.filter((movies) =>
+            movies.characters.includes('https://swapi-api.alx-tools.com/api/people/${characterId}/')
+        );
+        console.log('Number of movies whwere Wedge Antilles is present: ${wedgeAntillesMovies.length}');
+    
+    } else {
+        console.log('Error: Unable to fetch movie data. Status Code: ${response.statusCode}');
+        process.exit(1);
+    }
+});
