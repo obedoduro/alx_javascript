4ffa524f12d6dea44f6b9b4023092809324cea68
Obed Oduro <oduroadjeiobed16@gmail.com>
2023-10-10 12:45:02 +0000

    how many cmpleted

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -31,7 +31,7 @@ request(apiUrl, (error, response, body) => {
 
         // Print the users with completed task counts
         for (const userId in completedTasksByUser) {
-            if (count >= 8) {
+            if (count <= 8) {
                 console.log(`'${userId}': ${completedTasksByUser[userId]},`);
                 count = count + 1;
             }
